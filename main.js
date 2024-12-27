74aa68e4c013e33311beed9092e228c39289b43e
Ignacio Mercado <igmercado46@gmail.com>
2024-12-27 03:10:00 -0300

    positions stickman to the windows absolute center

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 console.log("main.js loaded")
+const $IMAGE = document.getElementById("image")
+
+$IMAGE.style.left = window.innerWidth/2 + "px"
+$IMAGE.style.top = window.innerHeight/2 + "px"
 
 addEventListener("click", (e) => {
     console.log(e.clientX, e.clientY)
